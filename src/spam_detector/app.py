097cbfbdebfea433de3ff376b55e0723aab66fde
mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>
2024-10-17 16:19:12 +0200

    feat(api): formalise root route
    - pass test case
    - add interface HealthResponse
    
    Signed-off-by: mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>

--- src/spam_detector/app.py
+++ src/spam_detector/app.py
@@ -4,18 +4,22 @@ The entrypoint for the FastAPI application.
 
 from fastapi import FastAPI
 
+from spam_detector.interfaces import HealthResponse
+
 app = FastAPI()
+
+APP_VERSION = "0.1.0"
 
 
 @app.get("/")
-def read_root():
+def read_root() -> HealthResponse:
     """
     The root endpoint for the FastAPI application.
 
     Returns the current version and the status of the application.
     """
 
-    raise NotImplementedError("Not implemented yet")
+    return HealthResponse(version=APP_VERSION, status="ok")
 
 
 @app.post("/detect_text")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,41 @@
+import pytest
 from fastapi.testclient import TestClient
-from main import app
 
-client = TestClient(app)
-
-
-def test_read_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"version": "0.1.0", "status": "ok"}
+from spam_detector.app import app
 
 
-def test_detect_text():
-    response = client.post("/detect_text")
-    assert response.status_code == 501
-    assert response.json() == {"detail": "Not implemented yet"}
+class TestApp:
+    """
+    Tests the FastAPI application
+    """
 
+    @pytest.fixture
+    def client(self):
+        """
+        Creates a TestClient instance for the FastAPI application.
+        """
+        return TestClient(app)
 
-def test_process_page():
-    response = client.post("/process_page")
-    assert response.status_code == 501
-    assert response.json() == {"detail": "Not implemented yet"}
+    def test_read_root(self, client):
+        """
+        Tests the root endpoint of the FastAPI application.
+        """
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"version": "0.1.0", "status": "ok"}
+
+    def test_detect_text(self, client):
+        """
+        Tests the detect_text endpoint of the FastAPI application.
+        """
+        response = client.post("/detect_text")
+        assert response.status_code == 501
+        assert response.json() == {"detail": "Not implemented yet"}
+
+    def test_process_page(self, client):
+        """
+        Tests the process_page endpoint of the FastAPI application.
+        """
+        response = client.post("/process_page")
+        assert response.status_code == 501
+        assert response.json() == {"detail": "Not implemented yet"}
