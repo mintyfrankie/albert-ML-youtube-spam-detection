493670a3e42836b1813abbf66343bff0ee3d4c39
mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>
2024-10-19 22:41:09 +0200

    build(test): add api server tests
    Signed-off-by: mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,26 +20,54 @@ class TestApp:
         """
         Tests the root endpoint of the FastAPI application.
         """
-        response = client.get("/")
+        response = client.get("/v1/health")
+        data = response.json()
+
         assert response.status_code == 200
-        assert response.json() == {"version": "0.1.0", "status": "ok"}
+        assert data["status"] is not None
+        assert data["version"] is not None
 
     def test_detect_text(self, client):
         """
         Tests the detect_text endpoint of the FastAPI application.
         """
 
-        # TODO: implement the test
-        response = client.post("/detect_text")
-        assert response.status_code == 501
-        assert response.json() == {"detail": "Not implemented yet"}
+        payload = {"content": "this is an example YouTube comment."}
+        response = client.post("/v1/detect", json=payload)
+        data = response.json()
+
+        assert response.status_code == 200
+        assert data["is_spam"] is not None
+
+    def test_detect_with_wrong_payload(self, client):
+        """
+        Tests the detect_text endpoint of the FastAPI application with wrong payload.
+        """
+
+        payload = {"wrong": "field"}
+        response = client.post("/v1/detect", json=payload)
+
+        assert response.status_code == 422
 
     def test_process_page(self, client):
         """
         Tests the process_page endpoint of the FastAPI application.
         """
 
-        # TODO: implement the test
-        response = client.post("/process_page")
-        assert response.status_code == 501
-        assert response.json() == {"detail": "Not implemented yet"}
+        payload = {"id": "dQw4w9WgXcQ", "first": 10}
+        response = client.post("/process_page", json=payload)
+        data = response.json()
+
+        assert response.status_code == 200
+        assert payload["first"] == data["nb"]
+        assert len(data["comments"]) == payload["first"]
+
+    def test_process_page_with_wrong_payload(self, client):
+        """
+        Tests the process_page endpoint of the FastAPI application with wrong payload.
+        """
+
+        payload = {"wrong": "field"}
+        response = client.post("/process_page", json=payload)
+
+        assert response.status_code == 422
