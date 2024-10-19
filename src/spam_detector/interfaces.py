83543ccee571e5aed0605bacb00c1a70a99301b2
mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>
2024-10-19 22:41:26 +0200

    feat(interfaces): add api endpoints interfaces
    Signed-off-by: mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>

--- src/spam_detector/interfaces.py
+++ src/spam_detector/interfaces.py
@@ -14,15 +14,32 @@ class HealthResponse(BaseModel):
     status: str = Field(..., description="Status of the service")
 
 
-class Comment(BaseModel):
+class DetectRequestPayload(BaseModel):
     """Request model for comment detection."""
 
-    uuid: UUID = Field(..., description="Unique identifier of the comment")
+    uuid: UUID = Field(None, description="Unique identifier of the comment")
     content: str = Field(..., description="Content of the comment")
 
 
-class CommentDetectionResponse(BaseModel):
+class DetectResponse(BaseModel):
     """Response model for comment detection."""
 
     uuid: UUID = Field(..., description="Unique identifier of the comment")
     is_spam: bool = Field(..., description="Whether the comment is spam or not")
+
+
+class VideoRequestPayload(BaseModel):
+    """Request model for video comment processing."""
+
+    id: str = Field(..., description="YouTube video ID")
+    first: int = Field(50, description="Number of comments to process")
+
+
+class VideoResponse(BaseModel):
+    """Response model for video comment processing."""
+
+    id: str = Field(..., description="YouTube video ID")
+    nb: int = Field(..., description="Number of comments processed")
+    comments: list[DetectResponse] = Field(
+        ..., description="List of comment detections"
+    )
