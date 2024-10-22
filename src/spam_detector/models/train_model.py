268c104d9d5e1667534c080d7cd674168e2c709c
mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>
2024-10-22 13:31:15 +0200

    feat(model): add etl pipeline
    Signed-off-by: mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>

--- src/spam_detector/models/train_model.py
+++ src/spam_detector/models/train_model.py
@@ -13,17 +13,40 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 
 DATA_DIR = Path(__file__).parent / "data"
+DUMP_DIR = Path(__file__).parent
 
-# TODO: build an ETL pipeline to load data, merge data, and save to a new csv file
+
+def etl(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    ETL pipeline to load data, merge data, and save to a new csv file
+    """
+
+    TARGET_COLUMNS = ["content", "label"]
+    COLUMNS_RENAME_MAPPING = {
+        "CONTENT": "content",
+        "Comment": "content",
+        "CLASS": "label",
+        "Spam": "label",
+    }
+
+    df = df.copy()
+    df = df.rename(columns=COLUMNS_RENAME_MAPPING)
+    df = df[TARGET_COLUMNS]
+    df = df.dropna(subset=TARGET_COLUMNS)
+    df["content"] = df["content"].str.strip()
+    df["label"] = df["label"].astype(int)
+
+    return df
+
+
 df_1 = pd.read_csv(DATA_DIR / "youtube_spam.csv")
-df_1 = df_1[["CONTENT", "CLASS"]]
 df_2 = pd.read_csv(DATA_DIR / "yt_comments_5000.csv", encoding="cp1252")
-df_2 = df_2.rename(columns={"Comment": "CONTENT", "Spam": "CLASS"})
-
+df_1 = etl(df_1)
+df_2 = etl(df_2)
 df = pd.concat([df_1, df_2], ignore_index=True)
 
-X = df["CONTENT"]
-y = df["CLASS"]
+X = df["content"]
+y = df["label"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=13
@@ -57,6 +80,5 @@ pipeline.fit(X_train, y_train)
 # TODO: determine threshold
 # TODO: implement mlflow
 
-DUMP_DIR = Path(__file__).parent / "dump"
 
-dump(pipeline, DUMP_DIR / "spam_detector.joblib")
+dump(pipeline, DUMP_DIR / "model.joblib")
