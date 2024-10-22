5a4fe06aa434b56813cc549d9ca4b359099db260
mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>
2024-10-22 10:12:12 +0200

    feat(api): add CORS policy
    Signed-off-by: mintyfrankie <77310871+mintyfrankie@users.noreply.github.com>

--- src/spam_detector/app.py
+++ src/spam_detector/app.py
@@ -6,6 +6,7 @@ from typing import List
 from uuid import UUID
 
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 from spam_detector.interfaces import (
     DetectRequestPayload,
@@ -16,6 +17,15 @@ from spam_detector.interfaces import (
 from spam_detector.services import detect_spam, get_youtube_comments
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 APP_VERSION = "0.0.2"
 
